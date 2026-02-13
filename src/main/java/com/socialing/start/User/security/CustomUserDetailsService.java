9aa6214d02c7678fe8dc10b3eefeb451fea1df62
skbnsl <sagar.b@xgenpluc.com>
2026-02-13 19:31:36 +0530

    working on security

---
+++ src/main/java/com/socialing/start/User/security/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.socialing.start.User.security;
+
+import com.socialing.start.User.entity.User;
+import com.socialing.start.User.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
